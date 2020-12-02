9f952483f0e676a2d49ecd6deb8a17da093a7461
Marin Tsanov <btg.tsanov.marin@gmail.com>
2020-12-03 00:02:45 +0200

    The logout page was actually needed (undo delete logout.html and .js)

---
+++ scripts/logout.js
@@ -0,0 +1,26 @@
+function logoutUser()
+{
+    $.ajax({
+        type: 'GET',
+        url: APIConfig.host + '/user',
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader ("Authorization", "Basic " + btoa(":"));
+        },
+        success: function (result) {
+            $("#logoutMessage").text("Log out not successful.");
+        },
+        error: function (xhr, status, error)
+        {
+            if(xhr.status == 401)
+            {
+                window.location.href = "login.html";
+            }
+            else 
+                $("#logoutMessage").text("An error has occurred during the log out procedure.");
+        }
+    });
+}
