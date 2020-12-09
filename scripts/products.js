38509fa47009e782c0368b255f257881982da102
Marin Tsanov <btg.tsanov.marin@gmail.com>
2020-12-10 00:17:05 +0200

    Created products listing page

--- scripts/products.js
+++ scripts/products.js
@@ -1,3 +1,6 @@
+let pageLoaded = false;
+let serverResponse;
+
 //a script to validate all the inputs when they get clicked out
 $('input').blur(function(evt) {
     evt.target.checkValidity();
@@ -11,6 +14,11 @@ $.ajax({
     },
     crossDomain: true,
     url: APIConfig.host + '/user',
+    success: function(result) {
+        serverResponse = result;
+        if(pageLoaded)
+            establishUser();
+    },
     error: function(xhr) {
         if(xhr.status == 401)
             window.location.href = "../login.html";
@@ -19,9 +27,18 @@ $.ajax({
     }
 });
 
-//function to fetch all products from the Rest API
-function fetchProducts()
+//function to fetch all products from the Rest API and perform some user-specific operations
+function productsIndexPageLoaded()
 {
+    pageLoaded = true;
+    //switch to the theme mode accordingly
+    if(getCookie("techstoreDashboardMode") == "dark")
+        toggleThemeMode(false);
+    
+    //establish an authenticated user
+    if(serverResponse)
+        establishUser();
+
     $.ajax({
         type: 'GET',
         xhrFields: {
@@ -29,9 +46,94 @@ function fetchProducts()
         },
         crossDomain: true,
         url: APIConfig.host + '/products',
-        success: showProductsListing,
+        success: function(result) {
+            showProductsListing(result);
+            createDataTable();
+        },
         error: showProductsError
     });
+}
+
+//function to establish the user with their rank
+function establishUser()
+{
+    if(serverResponse)
+        $("#profileDropdown").text(serverResponse.rank);
+
+    switch(serverResponse.rank)
+    {
+        case "Merchant":
+            $("#clients").show();
+            $("#sales-merchant").show();
+            break;
+        case "Administrator":
+            $("#products").show();
+            $("#merchants").show();
+            $("#sales-admin").show();
+            break;
+    }
+}
+
+//function to switch between light & dark mode
+function toggleThemeMode(clicks)
+{
+    $('h4, button').not('.check, .btn-primary, .btn-default, .btn-secondary, .btn-success, .btn-info, .btn-warning, .btn-danger').toggleClass('dark-grey-text text-white');
+    $('.list-panel a').toggleClass('dark-grey-text');
+
+    $('footer, .card').toggleClass('dark-card-admin');
+    $('#welcomeCard').removeClass('dark-card-admin'); //welcome card should not be darkened
+    $('.blue-gradient').toggleClass('pinot-noir-gradient');
+    $('.calm-darya-gradient').toggleClass('ash-gradient');
+    $('body, .navbar').toggleClass('white-skin navy-blue-skin');
+    $('#dark-mode').toggleClass('white text-dark btn-outline-black');
+    $('body').toggleClass('dark-bg-admin');
+    $('h6, .card, p, td, th, i, li a, h4, input, label').not(
+        '#slide-out i, #slide-out a, .dropdown-item i, .dropdown-item, .btn-secondary').toggleClass('text-white');
+    $('.btn-dash').toggleClass('grey blue').toggleClass('lighten-3 darken-3');
+    $('.gradient-card-header').toggleClass('white black lighten-4');
+    $('.list-panel a').toggleClass('navy-blue-bg-a text-white').toggleClass('list-group-border');
+
+    if(clicks && (!getCookie("techstoreDashboardMode") || getCookie("techstoreDashboardMode") == "bright"))
+        setCookie("techstoreDashboardMode", "dark", 365);
+
+    else if(clicks && (getCookie("techstoreDashboardMode") == "dark"))
+        setCookie("techstoreDashboardMode", "bright", 365);
+}
+
+//function to create the data table
+function createDataTable()
+{
+    let table = $('#productsListingTable').DataTable();
+    $('#productsListingTable_wrapper').find('label').each(function () {
+        $(this).parent().append($(this).children());
+    });
+    $('#productsListingTable_wrapper .dataTables_filter').find('input').each(function () {
+        const $this = $(this);
+        $this.attr("placeholder", "Search");
+        $this.removeClass('form-control-sm');
+    });
+    $('#productsListingTable_wrapper .dataTables_length').addClass('d-flex flex-row');
+    $('#productsListingTable_wrapper .dataTables_filter').addClass('md-form');
+    $('#productsListingTable_wrapper select').removeClass(
+    'custom-select custom-select-sm form-control form-control-sm');
+    $('#productsListingTable_wrapper select').addClass('mdb-select');
+    $('#productsListingTable_wrapper .mdb-select').materialSelect();
+    $('#productsListingTable_wrapper .dataTables_filter').find('label').remove();
+
+    //add dark mode to all newly added components as well
+    darkModeSwitchTextCompletely();
+
+    table.on('draw', function (e) {
+        darkModeSwitchTextCompletely();
+    });
+}
+
+//function to make sure all new content is also in dark mode
+function darkModeSwitchTextCompletely()
+{
+    if(getCookie("techstoreDashboardMode") == "dark")
+        $('h6, .card, p, td, th, i, li a, h4, input, label').not(
+            '#slide-out i, #slide-out a, .dropdown-item i, .dropdown-item, .text-white, .btn-secondary').toggleClass('text-white');
 }
 
 //setting up the product editing page
@@ -164,16 +266,18 @@ function showProductsListing(result)
 
     columnHeaders.forEach((header) => {
         $("#productsTableHeaders").append("<th>" + header + "</th>");
+        $("#productsTableFooters").append("<th>" + header + "</th>");
     });
 
     result.forEach((row) => {
-        $("#productsTableRows").append("<tr>");
+        let toAppend = "<tr>";
 
         for (const [key, element] of Object.entries(row))
-            $("#productsTableRows").append("<td>" + element + "</td>");
+            toAppend += "<td>" + element + "</td>";
 
-        $("#productsTableRows").append("<td><a href='edit.html?product=" + row.id + "'><button>Edit</button></a> <button onclick='deleteProduct(" + row.id + ")'>Delete</button></td>");
-        $("#productsTableRows").append("</tr>");
+        toAppend += "<td><a href='edit.html?product=" + row.id + "'><button class='btn btn-primary btn-rounded waves-effect waves-light'>Edit</button></a> <button class='btn btn-rounded btn-danger waves-effect waves-light' onclick='deleteProduct(" + row.id + ")'>Delete</button></td>";
+        toAppend += "</tr>";
+        $("#productsTableRows").append(toAppend);
     });
 }
 
@@ -210,4 +314,4 @@ function getUrlParameter(sParam)
             return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
         }
     }
-};
+}
